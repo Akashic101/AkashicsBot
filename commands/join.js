1f420b823e8106c4d9328ad2903b8c8cc944dfba
David Moll <david.leander.moll@gmail.com>
2020-12-16 19:20:57 +0100

    Added module-infos

--- commands/join.js
+++ commands/join.js
@@ -3,6 +3,9 @@
 
 module.exports = {
 	name: `join`,
+	description: "Emits the event guildMemberAdd",
+	aliases: ['guildmemberadd'],
+	color: '#1f55c1',
 	execute(client, message, args) {
 		message.client.emit(`guildMemberAdd`, message.member);
 	},
